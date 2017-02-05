ba8ae389c343c0c251617e27595104cd25bcffc2
zhutianhua-qq <278610562@qq.com>
2017-02-05 21:54:31 +0800

    delete SIMPLE workmode

--- script/socket/short_connection_flymode/main.lua
+++ script/socket/short_connection_flymode/main.lua
@@ -1,6 +1,8 @@
 PROJECT = "SOCKET_SHORT_CONNECTION_FLYMODE"
 VERSION = "1.0.0"
 require"sys"
+--关闭脚本中的所有trace打印
+--sys.opntrace(false)
 require"link"
 require"linkapp"
 require"dbg"
@@ -12,5 +14,5 @@ sys.init(0,0)
 --ril.request("AT*TRACE=\"DSS\",1,0")
 --ril.request("AT*TRACE=\"RDA\",1,0")
 --设置工作模式为简单模式
-sys.setworkmode(sys.SIMPLE_MODE)
+--sys.setworkmode(sys.SIMPLE_MODE)
 sys.run()
