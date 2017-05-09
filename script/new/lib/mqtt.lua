e9d0fdbf8490e771f7b6d0a695be69314c89e9f8
zhutianhua-qq <278610562@qq.com>
2017-05-09 16:36:00 +0800

    修改注释

--- script/new/lib/mqtt.lua
+++ script/new/lib/mqtt.lua
@@ -495,10 +495,10 @@ end
 函数名：ntfy
 功能  ：socket状态的处理函数
 参数  ：
-        idx：number类型，linkapp中维护的socket idx，跟调用linkapp.sckconn时传入的第一个参数相同，程序可以忽略不处理
+        idx：number类型，socket中维护的socket idx，跟调用socket.connect时传入的第一个参数相同，程序可以忽略不处理
         evt：string类型，消息事件类型
 		result： bool类型，消息事件结果，true为成功，其他为失败
-		item：table类型，{data=,para=}，消息回传的参数和数据，目前只是在SEND类型的事件中用到了此参数，例如调用linkapp.scksnd时传入的第2个和第3个参数分别为dat和par，则item={data=dat,para=par}
+		item：table类型，{data=,para=}，消息回传的参数和数据，目前只是在SEND类型的事件中用到了此参数，例如调用socket.send时传入的第2个和第3个参数分别为dat和par，则item={data=dat,para=par}
 返回值：无
 ]]
 function ntfy(idx,evt,result,item)
@@ -695,7 +695,7 @@ end
 函数名：rcv
 功能  ：socket接收数据的处理函数
 参数  ：
-        idx ：linkapp中维护的socket idx，跟调用linkapp.sckconn时传入的第一个参数相同，程序可以忽略不处理
+        idx ：socket中维护的socket idx，跟调用socket.connect时传入的第一个参数相同，程序可以忽略不处理
         data：接收到的数据
 返回值：无
 ]]
