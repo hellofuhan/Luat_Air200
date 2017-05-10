eb2c531af776745c63275c640615a7f674504d2f
zhutianhua-qq <278610562@qq.com>
2017-05-10 23:28:23 +0800

    增加对UART口输出trace代码位置的调用说明

--- script/new/demo/xiaoman_gps_tracker/whole_test/main.lua
+++ script/new/demo/xiaoman_gps_tracker/whole_test/main.lua
@@ -5,8 +5,8 @@ PROJECT = "XIAOMAN_WHOLE_TEST"
 VERSION = "1.0.0"
 require"sys"
 --[[
-如果使用UART输出trace，打开这行注释的代码"--sys.opntrace(true,1)"即可，第2个参数1表示UART1输出trace，根据自己的修改这个参数
-这里是最早可以设置trace口的地方，如果开机其他的文件有语法错误，UART口能够输出语法错误信息
+如果使用UART输出trace，打开这行注释的代码"--sys.opntrace(true,1)"即可，第2个参数1表示UART1输出trace，根据自己的需要修改这个参数
+这里是最早可以设置trace口的地方，代码写在这里可以保证UART口尽可能的输出“开机就出现的错误信息”
 如果写在后面的其他位置，很有可能无法输出错误信息，从而增加调试难度
 ]]
 --sys.opntrace(true,1)
